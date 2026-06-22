929003b1d0d4b665d5cb7a1a4942047f0019313f
isaque570 <isaquevianna30@gmail.com>
2026-06-22 13:54:25 -0300

    Add files via upload

---
+++ 9. Análise de Crescimento Anual (2).js
@@ -0,0 +1,9 @@
+let alfa = 150
+let beta = 110
+let anos = 0 
+while(beta<alfa){
+    alfa += 4
+    beta += 9
+    anos += 1 
+}
+alert(`A empresa beta ultrapassou a empresa alpha em ${anos} anos`)
