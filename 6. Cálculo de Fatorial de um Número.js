91e59b1942e052cbe3e52b9a00446640de86037a
isaque570 <isaquevianna30@gmail.com>
2026-06-22 14:07:35 -0300

    Update 6. Cálculo de Fatorial de um Número.js

--- 6. Cálculo de Fatorial de um Número.js
+++ 6. Cálculo de Fatorial de um Número.js
@@ -1,6 +1,9 @@
-let n = Number(prompt("Digite um número: "))
+function fatorial(n){
 let fatorial = 1
 for(let i=n;i>=1;i--){
     fatorial = fatorial*i
 }
 alert(`o fatorial do número ${n} é: ${fatorial}`)
+}
+let n = Number(prompt("Digite o número que deseja ver o fatorial: "))
+let f = fatorial(n)
